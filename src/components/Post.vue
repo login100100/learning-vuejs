beb7365eacd5e03735fca4fca5a37c04f13c2b92
marcin.radomski <login100100100@gmail.com>
2019-08-13 22:46:17 +0200

    Divided single post component

---
+++ src/components/Post.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p>{{ title }}</p>
+        <p>{{ description }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Post extends Vue {
+    @Prop(String) readonly title?: string;
+    @Prop(String) readonly description?: string;
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,21 +1,19 @@
 <template>
     <div class="container">
-        <div v-bind:key="post.id" v-for="post in posts">
-            <h2>
-                {{post.title}}
-            </h2>
-            <p>
-                {{post.description}}
-            </p>
-        </div>
+        <Post v-for="post in posts" v-bind:key="post.id" :title="post.title" :description="post.description"></Post>
     </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
 import PostsModule from '@/store/modules/Posts.module';
+import Post from '@/components/Post.vue';
 
-@Component
+@Component({
+    components: {
+        Post
+    }
+})
 export default class Posts extends Vue {
     // dynamic PostsModule have different syntax for accessing module parameters and methods
     get posts() {
